0bdd53996de700f7285ae9510a338e6860563706
John Ashley <johnashleyworkemail@gmail.com>
2021-10-09 16:35:05 -0400

    added pseudocode e41

---
+++ exercise41/src/main/java/Solution41.java
@@ -0,0 +1,51 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+read in the file and
+organize the information from the file into an order
+write the names/information into a new file
+ */
+
+
+package baseline;
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+
+
+
+public class Solution41 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+
+    }
+
+
+    private BufferedReader getInputInfo()
+    {
+
+    }
+
+    private List organizeNames()
+    {
+
+    }
+
+    private int countNames()
+    {
+
+    }
+
+
+    private BufferedWriter writeToOutput()
+    {
+
+    }
+
+}
