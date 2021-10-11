a7d65281f197273332d499db57bcf6fe93019699
John Ashley <johnashleyworkemail@gmail.com>
2021-10-11 15:07:27 -0400

    added pseudocode e43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,77 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+
+
+/*
+PSEUDOCODE
+ */
+
+
+
+package baseline;
+
+public class Solution43 {
+
+    public static void main(String[] args)
+    {
+        Solution43 solution = new Solution43();
+    }
+
+
+    private String getSiteName()
+    {
+        //ask for input for site name and return it
+        return "s";
+    }
+
+    private String getAuthorName()
+    {
+        //prompt for input for author name and return it
+        return "s";
+    }
+
+    private boolean checkForJavaScriptFolder()
+    {
+        //ask if the user wants a folder for javascript
+
+        //return true if response is y. false for anything else
+        return true;
+    }
+
+    private boolean checkForCSSFolder()
+    {
+        //ask if the user wants a folder for CSS
+
+        //return true if response is y. false for anything else.
+        return true;
+    }
+
+    public void createHTMLFile(String siteName, String authorName, boolean javaScript, boolean cssFolder)
+    {
+
+
+        //use bufferedwriter to create file
+
+        //add any necessary heading to the file
+
+        //set the name of the site inside the title tag
+
+
+        //set the author name of the site in the meta tag
+
+
+        //create javascript folder if needed
+
+        //create css folder if needed
+
+
+        //close file
+
+
+
+    }
+
+}
