16daa0f8bbcc7a1cc1ee4b671a48868e12a1984b
John Ashley <johnashleyworkemail@gmail.com>
2021-10-12 16:52:50 -0400

    added pseudocode e45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,46 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+package baseline;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Solution45 {
+
+    public static void main(String[] args)
+    {
+        Solution45 solution = new Solution45();
+    }
+
+
+    public String getInputInfo()
+    {
+        //use bufferedReader to get input from file
+
+
+        //set the input file to a new string
+
+
+        //return the string
+    }
+
+    public String replaceWord()
+    {
+        //set an old word and new word variable
+
+        //replace any words within the string if it contains the string
+    }
+
+    public void makeOutput()
+    {
+        //create an output file
+
+        //print the new string to the file
+    }
+
+
+
+
+}
