77c5d69a49a96e3fef1445e753c35029f158b7c2
John Ashley <johnashleyworkemail@gmail.com>
2021-10-14 14:08:01 -0400

    added code for e43. Still fixing.

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -13,49 +13,111 @@ PSEUDOCODE
 
 package baseline;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Solution43 {
+
+    private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args)
     {
         Solution43 solution = new Solution43();
+
+        String siteName = solution.getSiteName();
+        String authorName = solution.getAuthorName();
+        boolean jsFolder = solution.checkForJavaScriptFolder();
+        boolean CSSFolder = solution.checkForCSSFolder();
+        String file = solution.createHTMLFile(siteName, authorName);
+        solution.createCSSFolder(siteName, CSSFolder);
+        solution.createJavaScriptFolder(siteName, jsFolder);
+
     }
 
 
     private String getSiteName()
     {
         //ask for input for site name and return it
-        return "s";
+        System.out.println("Site name: ");
+        return input.nextLine();
     }
 
     private String getAuthorName()
     {
         //prompt for input for author name and return it
-        return "s";
+        System.out.println("Author name: ");
+        return input.nextLine();
     }
 
     private boolean checkForJavaScriptFolder()
     {
         //ask if the user wants a folder for javascript
+        System.out.println("Do you want a folder for JavaScript? Enter y for yes. Anything else for no.");
+
+        String answer = input.nextLine();
 
         //return true if response is y. false for anything else
-        return true;
+        if(answer.equals("y"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     private boolean checkForCSSFolder()
     {
         //ask if the user wants a folder for CSS
+        System.out.println("Do you want a folder for CSS? Enter y for yes. Anything else for no.");
 
+        String answer = input.nextLine();
         //return true if response is y. false for anything else.
-        return true;
+        if(answer.equals("y"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    public void createHTMLFile(String siteName, String authorName, boolean javaScript, boolean cssFolder)
+    public String createHTMLFile(String siteName, String authorName)
     {
 
+        String newFile = ("""
+        <!doctype html>
+        <html>
+        <head>
+        <title>$siteName</title>
+        <meta name = "author" content = "$authorName" />
+        </head>
+        <body>
+        <p>PogU</p>
+        </body>
+        </html>""".replace("$siteName", siteName).replace("$authorName", authorName));
 
-        //use bufferedwriter to create file
 
-        //add any necessary heading to the file
+        File file = new File(".data/myWebsite/" + siteName + "/index.html");
+
+
+        try
+        {
+            BufferedWriter output = new BufferedWriter(new FileWriter(file));
+
+            output.write(newFile);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        return newFile;
 
         //set the name of the site inside the title tag
 
@@ -71,6 +133,27 @@ public class Solution43 {
         //close file
 
 
+
+    }
+
+    public void createJavaScriptFolder(String siteName, boolean js)
+    {
+        if(js)
+        {
+            File jsFolder = new File(".data/myWebsite/" + siteName + "/js");
+
+            jsFolder.mkdirs();
+        }
+    }
+
+    public void createCSSFolder(String siteName, boolean CSS)
+    {
+        if(CSS)
+        {
+            File CSSFolder = new File(".data/myWebsite/" + siteName + "/js");
+
+            CSSFolder.mkdirs();
+        }
 
     }
 
