7951ad7d29656abd3a60f872883cb870fc129822
John Ashley <johnashleyworkemail@gmail.com>
2021-10-14 14:31:03 -0400

    added pseudocode e46

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,52 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+create map using strings/integer as a count
+read through file adding to map. use if statement to check if a word exists
+sort map by greatest number of appearances/integer values
+ */
+
+
+package baseline;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution46 {
+
+    public static void main(String[] args)
+    {
+        Solution46 solution = new Solution46();
+    }
+
+    private Map<String, Integer> getInputWordFrequency()
+    {
+        //create string-integer map
+        Map<String, Integer> wordFrequency = new HashMap<>();
+
+        //read in file and scan for words
+
+        //if the word doesn't exist, add it. if it does exist, increase integer by 1.
+
+        return wordFrequency;
+    }
+
+    private Map<String, Integer> sortByMostUsedWord(Map<String, Integer> WordFrequency)
+    {
+
+        //use map entry to sort in order
+
+        return WordFrequency;
+    }
+
+    private void printOutput(Map<String, Integer> wordFrequency)
+    {
+
+    }
+
+
+}
