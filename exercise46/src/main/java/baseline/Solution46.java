91a8f28dbcc34422121fc5476b7ffc56c8302625
John Ashley <johnashleyworkemail@gmail.com>
2021-10-14 15:00:08 -0400

    added code for e46. Add testing/uml later.

--- exercise46/src/main/java/baseline/Solution46.java
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -13,14 +13,21 @@ sort map by greatest number of appearances/integer values
 package baseline;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Solution46 {
 
     public static void main(String[] args)
     {
         Solution46 solution = new Solution46();
+
+        Map<String, Integer> wordFrequency = solution.getInputWordFrequency();
+
+        wordFrequency = solution.sortByMostUsedWord(wordFrequency);
+
+        solution.printOutput(wordFrequency);
     }
 
     private Map<String, Integer> getInputWordFrequency()
@@ -29,24 +36,66 @@ public class Solution46 {
         Map<String, Integer> wordFrequency = new HashMap<>();
 
         //read in file and scan for words
+        try(Scanner input = new Scanner(Paths.get("data/exercise46_input.txt")))
+        {
+            while(input.hasNext())
+            {
+                //get next word
+                String word = input.next();
+
+                //compare to other words in the map
+                if(wordFrequency.containsKey(word))
+                {
+                    //increase value of word by 1
+                    wordFrequency.put(word, wordFrequency.get(word)+1);
+                }
+                else
+                {
+                    //add a new word to the map if it does not exist
+                    wordFrequency.put(word, 1);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
         //if the word doesn't exist, add it. if it does exist, increase integer by 1.
 
         return wordFrequency;
     }
 
-    private Map<String, Integer> sortByMostUsedWord(Map<String, Integer> WordFrequency)
+    private LinkedHashMap<String, Integer> sortByMostUsedWord(Map<String, Integer> wordFrequency)
     {
 
-        //use map entry to sort in order
+        //create a linked hash map (easier printing later)
+        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
 
-        return WordFrequency;
+        //sort map
+        wordFrequency.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+
+        //return linked hash map
+        return sortedMap;
     }
 
     private void printOutput(Map<String, Integer> wordFrequency)
     {
+        for(Map.Entry<String, Integer> entry : wordFrequency.entrySet())
+        {
+            //get the string name/key of the map
+            String name = entry.getKey();
+            System.out.printf("%s: ", name);
+            //get the value/number of times key appeared
+            int count = entry.getValue();
 
+            //print a * per number of appearances
+            for(int i = 0; i < count; i++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
-
 
 }
