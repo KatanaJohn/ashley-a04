880023184407e40068823cb8ba0107184c608947
John Ashley <johnashleyworkemail@gmail.com>
2021-10-16 19:38:35 -0400

    added pseudocode e44

---
+++ exercise44/src/main/java/baseline/Product.java
@@ -0,0 +1,16 @@
+package baseline;
+
+//make things easier to work with multiple products and multiple variables and return those values
+public class Product {
+
+    String name;
+    double price;
+    int quantity;
+
+    public Product(String name, double price, int quantity)
+    {
+
+    }
+
+
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,51 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 John Ashley
+ */
+
+/*
+take product name as input
+possibly make product class
+retrieve price/quantity for product from a file
+ */
+
+
+package baseline;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+import java.util.List;
+
+public class Solution44 {
+
+    public static void main(String[] args)
+    {
+        Solution44 solution = new Solution44();
+    }
+
+
+    private String getInput()
+    {
+        //ask and return input
+
+        return "s";
+    }
+
+
+    //transfer input info from json to list of products
+    private List<Product> getFileInput()
+    {
+
+    }
+
+
+    //search for the product name asked and return its values
+    private Product searchForProduct(String searchedProduct)
+    {
+
+    }
+
+
+
+}
